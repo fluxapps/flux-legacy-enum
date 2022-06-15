3377db9d03f835114f59e11b66f01fa77c3fa3f6
fluxfw <fluxfw@fluxlabs.ch>
2022-06-15 12:23:14 +0200

    static return type

--- src/Backed/IntBackedEnum.php
+++ src/Backed/IntBackedEnum.php
@@ -7,14 +7,8 @@ use FluxLegacyEnum\Unit\UnitEnum;
 interface IntBackedEnum extends UnitEnum, IntBackedEnumValue
 {
 
-    /**
-     * @return static
-     */
     public static function from(int $value) : static;
 
 
-    /**
-     * @return static|null
-     */
     public static function tryFrom(int $value) : ?static;
 }
--- src/Backed/StringBackedEnum.php
+++ src/Backed/StringBackedEnum.php
@@ -7,14 +7,8 @@ use FluxLegacyEnum\Unit\UnitEnum;
 interface StringBackedEnum extends UnitEnum, StringBackedEnumValue
 {
 
-    /**
-     * @return static
-     */
     public static function from(string $value) : static;
 
 
-    /**
-     * @return static|null
-     */
     public static function tryFrom(string $value) : ?static;
 }
